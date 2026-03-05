d9729976f04168d833fc95d9d56b74b9d117a607
Antarktida <93553146+qwingel@users.noreply.github.com>
2026-03-05 22:40:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    # <-------- model -------->
+    # X = [[delta_rating, map_id, delta_map_wr_windowed, LAN_flag, h2h_shrunk + h2h_count]]
+    return 0
+if __name__ == '__main__':
+    main()
